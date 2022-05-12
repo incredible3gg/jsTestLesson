5fbb882b1f5250598ffeab12ed4cf65cd9a4307e
Calix Robertsonmeyer <calix@Xies-MBP.lan>
2022-05-12 10:45:11 -0700

    bruh

--- dayOfWeek.test.js
+++ dayOfWeek.test.js
@@ -1,18 +1,48 @@
+function testday(year,month,day,msg,eday) {
+	test(msg, () => {
+		const date = new dOW(year,month,day);
+
+		expect(date.dayName()).toBe(eday);
+	});
+};	
+
 const dOW = require('./dayOfWeek');
 
 test('Jan 1, 1AD is Saturday', () => {
-    const date = new dOW(1,1,1);
+	const date = new dOW(1,1,1);
     
-    expect(date.dayName()).toBe('Saturday');
+	expect(date.dayName()).toBe('Saturday');
 });
 
 
 test('Jan 2, 1AD is Sunday', () => {
-    const date = new dOW(1,1,2);
-    expect(date.dayName()).toBe('Sunday');
+	const date = new dOW(1,1,2);
+	expect(date.dayName()).toBe('Sunday');
 });
 
 test('Feb 9, 1AD is Wednesday', () => {
 	const date = new dOW(1,2,9);
-	expect(date.dayName()).toBe('Wednesday');
+	expect(date.dayName()).toBe('Wednesday')
 });
+
+test('Feb 12 of 1AD is a Saturday', () => {
+	const date = new dOW(1,2,12);
+	expect(date.dayName()).toBe('Saturday')
+});
+
+test('Feb 12 of 2AD is a Sunday', () =>  {
+        const date = new dOW(2,2,12);
+        expect(date.dayName()).toBe('Sunday')
+});
+
+
+
+/*
+      ()
+_   ,-""-,
+\\,'      '.
+ |_        |
+  '._____.' 
+	
+   Kettle
+*/
